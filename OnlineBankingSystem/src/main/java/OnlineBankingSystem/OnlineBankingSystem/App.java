7f63dda7e403c617617e483c2adc20335700aa66
tajinderjudge <singhtajinderpal@hotmial.com>
2023-07-21 22:45:54 -0600

    it is my first commit

---
+++ OnlineBankingSystem/src/main/java/OnlineBankingSystem/OnlineBankingSystem/App.java
@@ -0,0 +1,13 @@
+package OnlineBankingSystem.OnlineBankingSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
